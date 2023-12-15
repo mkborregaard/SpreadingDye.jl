aa567c08ec57e51dc9d91275de7849bc90088967
Michael Krabbe Borregaard <mkborregaard@sund.ku.dk>
2023-12-15 15:04:17 +0100

    Add initial

--- src/SpreadingDye.jl
+++ src/SpreadingDye.jl
@@ -1,5 +1,70 @@
 module SpreadingDye
 
-# Write your package code here.
+# the two types of neighborhood
+const rook = ((-1,0), (0, 1), (1, 0), (0, -1))
+const quen = Tuple((x,y) for x in -1:1, y in -1:1 if !(x == y == 0))
 
+# the internal range filling function
+function spreading_dye!(georange, finalrange, dom, start; algo = rook)
+    # is the start cell outside the domain?
+    dom[start...] || return dom 
+
+    fill!(georange, false)
+    # apply the starting cell
+    georange[start...] = true
+    edges = Set((start, start .+ nb) for nb in algo if on_domain(dom, start .+ nb))
+    rangesize = 1
+    newcell = edge = start
+
+    # grow the range one cell at a time
+    while (rangesize += 1) <= finalrange
+        grow!(georange, edges, dom, algo)
+    end
+end
+
+function center_cell(m::AbstractMatrix{<:Number})
+    range = 0
+    x = 0
+    y = 0
+    for i in axes(m, 1), j in axes(m, 2)
+        if isfinite(m[i,j]) && m[i, j] != 0
+            x += i
+            y += j
+            range += 1
+        end
+    end
+    round(Int, x/range), round(Int, y/range)
+end
+
+# is a point on the domain?
+on_domain(dom, point) = min(point...) > 0 && first(point) <= size(dom, 1) && last(point) <= size(dom, 2) && dom[point...]
+
+# use rejection sampling to get a random point on the domain
+function pick_random(dom)
+    x = (rand(axes(dom,1)), rand(axes(dom,2)))
+    while !dom[x...]
+        x = (rand(axes(dom,1)), rand(axes(dom,2)))
+    end
+    x
+end
+
+# the outward_facing function to do the spreading dye
+function spreading_dye(finalrange, dom, start = pick_random(dom); algo = rook)
+    georange = fill!(similar(dom, Bool), false)
+    spreading_dye!(georange, finalrange, dom, start; algo = rook)
+    georange
+end
+
+# grow the range one at a time
+function grow!(georange, edges, dom, algo)
+    edge, newcell = rand(edges)
+    pop!(edges, (edge, newcell))
+    georange[newcell...] = true
+    for nb in algo
+        neighbor = newcell .+ nb
+        on_domain(dom, neighbor) && !georange[neighbor...] && push!(edges, (newcell, neighbor))
+    end
+end
+
+export spreading_dye, spreading_dye!, pick_random, center_cell
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,69 @@
 using SpreadingDye
 using Test
 
-@testset "SpreadingDye.jl" begin
-    # Write your tests here.
+@testset "Simple example" begin
+    # create a new domain and fill it
+    dom = fill(true, 100, 100)
+    start = (50,50)
+    sd = spreading_dye(3000, dom, start)
+
+end
+
+@testset "Jesper's example" begin
+    using Rasters
+    dd = Raster("../data/domaene.tif")
+    dom = !=(missingval(dd)).(dd)
+    rr = Raster("../data/species_range.tif")
+    rangesize = count(isfinite, rr)
+    coords = collect(CartesianIndices(rr))[isfinite.(rr)]
+    centroid = center_cell(rr)
+    sd = spreading_dye(rangesize, dom, centroid)
+end
+
+@testset "South America birds empirical data" begin
+    # load libraries
+    using CSV, DataFrames
+    bd = CSV.read("../data/Samerica_birds_16Apr2010.txt", DataFrame, header = false)
+
+    # reshape data nand initialize objects
+    xa = extrema(bd.Column4)
+    ya = extrema(bd.Column5)
+    d = Dict{String, Int}()
+    longs = Dict{String, Int}()
+    lats = Dict{String, Int}()
+    dom = falses(Int(last(xa) - first(xa) + 2), Int(last(ya) - first(ya) + 2))
+    for row in eachrow(bd)
+        x = Int(row.Column4 - first(xa) + 2)
+        y = Int(row.Column5 - first(ya) + 2)
+        dom[x, y] = true
+        if !haskey(d, row.Column1)
+            d[row.Column1] = 0
+            longs[row.Column1] = 0
+            lats[row.Column1] = 0
+        end
+        d[row.Column1] += 1
+        longs[row.Column1] += x
+        lats[row.Column1] += y
+    end
+
+    centroids = Dict(sp => (longs[sp] ÷ d[sp], lats[sp] ÷  d[sp]) for sp in keys(d))
+
+    # do a spreading dye for the first species
+    sp = first(keys(d))
+    sd = spreading_dye(d[sp], dom, centroids[sp])
+    heatmap(range(xa...), range(ya...), sd)
+
+    # do it for all
+    function doall(dom, d, centroids)
+        rich = zeros(size(dom))
+        georange = fill(false, size(dom))
+        for sp in keys(d)
+            spreading_dye!(georange, d[sp], dom, centroids[sp])
+            rich .+= georange
+        end
+        rich[.!(dom)] .= NaN
+        rich
+    end
+
+    doall(dom, d, centroids)
 end
